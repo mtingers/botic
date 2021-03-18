be78a04200a3db16827f8ebb75189f436dc64136
Matthew Ingersoll <matth@mtingers.com>
2021-03-17 20:10:46 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='Botic',
-    version='0.0.7',
+    version='1.0.0',
     author='Matth Ingersoll',
     author_email='matth@mtingers.com',
     packages=find_packages(),
