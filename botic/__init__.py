9537de5b93da4370ee092c63ca1132a5b6d74865
Matthew Ingersoll <matth@mtingers.com>
2021-03-18 10:01:56 -0700

    Release 1.1.0

--- botic/__init__.py
+++ botic/__init__.py
@@ -1,3 +1,3 @@
 """__init__.py"""
-__version__ = '0.0.0'
+__version__ = '1.1.0'
 VERSION = __version__
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='Botic',
-    version='1.0.4',
+    version='1.1.0',
     author='Matth Ingersoll',
     author_email='matth@mtingers.com',
     packages=find_packages(),
