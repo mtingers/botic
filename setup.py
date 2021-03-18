9d674aa370f4f2ff0ab5758bf7cf4abadebc7cd2
Matthew Ingersoll <matth@mtingers.com>
2021-03-17 21:12:10 -0700

    Add MANIFEST.in

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='Botic',
-    version='1.0.2',
+    version='1.0.3',
     author='Matth Ingersoll',
     author_email='matth@mtingers.com',
     packages=find_packages(),
